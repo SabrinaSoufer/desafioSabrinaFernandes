0f07189a4190dff48e30dbeb42c2cb9ae02f10d6
Sabrina fernandes <sabrina.soufer15@gmail.com>
2022-02-02 23:46:48 -0300

    Implementar alterações js

--- SCRIPT.js
+++ SCRIPT.js
@@ -1,10 +1,10 @@
-const imagem1 = document.getElementById('personagen1');
+const imagem1 = document.getElementById('personagem1');
 const imagem2 = document.getElementById('personagem2');
 const imagem3 = document.getElementById('personagem3');
 const botao = document.querySelector('button');
-const nomeDoPersonagen1 = document.getElementById('nome1');
-const nomeDoPersonagen2 = document.getElementById('nome2');
-const nomeDoPersonagen3 = document.getElementById('nome3');
+const nomeDoPersonagem1 = document.getElementById('nome1');
+const nomeDoPersonagem2 = document.getElementById('nome2');
+const nomeDoPersonagem3 = document.getElementById('nome3');
 const especie1 = document.getElementById('species1');
 const especie2 = document.getElementById('species2');
 const especie3 = document.getElementById('species3');
@@ -42,7 +42,7 @@ pegarPersonagem1 = () =>{
     }).then((response) => response.json()).then((data) => {
         imagem1.src = data.image;
         imagem1.alt = data.name;
-        nomeDoPersonagen1.innerHTML = data.name;
+        nomeDoPersonagem1.innerHTML = data.name;
         especie1.innerHTML = data.species;
         condicao1.innerHTML = traduzirCondicao (data);
     });
@@ -53,24 +53,23 @@ pegarPersonagem1 = () =>{
          Accept: 'application/json',
          "Content-type": 'application/json'
      }
-    }).then((response) => response.json().then((data) => {
+    }).then((response) => response.json()).then((data) => {
         imagem2.src = data.image;
         imagem2.alt = data.name;
-        nomeDoPersonagen2.innerHTML = data.name;
+        nomeDoPersonagem2.innerHTML = data.name;
         especie2.innerHTML = data.species;
         condicao2.innerHTML = traduzirCondicao(data);
     });
-
     fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio3}`, {
      method: 'GET',
      headers: {
          Accept: 'application/json',
          "Content-type": 'application/json'
      }
-    }).then((response) =>.json().then((data) => {
+    }).then((response) => response.json()).then((data) => {
         imagem3.src = data.image;
         imagem3.alt = data.name;
-        nomeDoPersonagen3.innerHTML = data.name;
+        nomeDoPersonagem3.innerHTML = data.name;
         especie3.innerHTML = data.species;
         condicao3.innerHTML = traduzirCondicao(data);
     });
